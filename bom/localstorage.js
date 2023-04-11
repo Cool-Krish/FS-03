4c23465baea5c057364fff38bb37d4609decf0c6
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-04-11 22:33:20 +0530

    :localStorage, session storage, set timeout and intervel:

---
+++ bom/localstorage.js
@@ -0,0 +1,68 @@
+// localStorage.setItem("myCat", "Tom");
+// localStorage.setItem("myMouse", "Jerry");
+// const cat = localStorage.getItem("myCat");
+// localStorage.removeItem("myCat");
+// localStorage.clear();
+
+// let arr = [{ a: 100 }, { b: 200 }, { c: 300 }];
+// localStorage.setItem("myArr", JSON.stringify(arr));
+// let data = localStorage.getItem("myArr");
+// console.log(JSON.parse(data)[0]);
+
+
+// sessionStorage.setItem("myCat", "Tom");
+// sessionStorage.setItem("myMouse", "Jerry");
+// const cat = sessionStorage.getItem("myCat");
+// sessionStorage.removeItem("myCat");
+// sessionStorage.clear();
+
+if (!sessionStorage.pageLoadCount) sessionStorage.pageLoadCount = 0;
+sessionStorage.pageLoadCount = parseInt(sessionStorage.pageLoadCount) + 1;
+document.getElementById("count").textContent = sessionStorage.pageLoadCount;
+let myData1 = Math.random() * 100;
+let myData2 = Math.random() * 100;
+let myData3 = Math.random() * 100;
+let id = setTimeout(cb, 3000, myData1, myData2, myData3);
+console.log(id);
+function cb(...data) {
+    console.log(data);
+}
+
+
+const alarm = {
+  remind(aMessage) {
+    alert(aMessage);
+    this.timeoutID = undefined;
+},
+
+  setup() {
+    if (typeof this.timeoutID === "number") {
+      this.cancel();
+        console.log(this.timeoutID);
+    }
+
+    this.timeoutID = setTimeout(
+      (...msg) => {
+        this.remind(msg);
+      },
+      1000,
+      "Wake up!","It is Morning","get ready"
+    );
+  },
+
+  cancel() {
+    clearTimeout(this.timeoutID);
+  },
+};
+// window.addEventListener("click", () => alarm.setup());
+
+let timer;
+document.querySelector(".start").addEventListener("click",()=>{
+    timer = setInterval(() => console.log("hello"), 1000);
+    console.log(timer);
+})
+
+document.querySelector(".stop").addEventListener("click",()=>{
+    console.log(timer);
+    clearInterval(timer);
+})
