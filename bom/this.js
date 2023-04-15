369a725c6aa18f6c64deeec7ba17a360a47d3344
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-04-15 22:40:17 +0530

    :fire: this interview questions leaderboard

--- bom/this.js
+++ bom/this.js
@@ -35,4 +35,72 @@ z.fn=y
 //z.fn()
 //console.log(x)
 let foo = x.fn
-foo()
+//foo()
+
+
+function type1(){
+    console.log(`I love ${this.name}`)
+}
+var name="krishna"
+type1()
+
+function type2(){
+    console.log(this.fname)
+}
+var fname = "Nitin"
+var ob={
+    fname: "manish",
+    type2: function (){
+        console.log(this.fname)
+    }
+}
+ob.type2()
+var food = "pizza"
+var obj ={
+    food: "pasta",
+    eat: function(){
+        console.log(this.food)
+    }
+}
+
+var foos = obj.eat
+foos()
+
+var length =1
+ function sq(){
+    let cb = function (){
+        console.log(this.length)
+    }
+    setTimeout(cb,2000)
+ }
+
+ var object = {
+    length: 3,
+    sq
+    
+ }
+
+ object.sq()
+
+function Name(frname,lname){
+    this.firstname = frname
+    this.lastname = lname
+}
+
+var classobj = new Name("Rohit","Sharma")
+
+console.log(classobj)
+
+
+
+
+let fs = {
+    c:"italy",
+    m:"pizz",
+    say(){
+        let fv = function (){console.log(this.m)}
+        fv()
+    }
+
+}
+fs.say()
