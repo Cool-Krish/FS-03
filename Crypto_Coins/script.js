86a9d9f5c38a48fa5b8c37fa1c7c648fe512004b
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-04-24 22:30:46 +0530

    :tada: crypto css

--- Crypto_Coins/script.js
+++ Crypto_Coins/script.js
@@ -1,40 +1,60 @@
-
-// const axios = require('axios');
-// const API_KEY = '05869e79-c079-4660-b857-57d3dd51e92e'
-
-
-// // const req = await axios.get("")
-// let response = null;
-// new Promise(async (resolve, reject) => {
-//   try {
-//     response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-//       headers: {
-//         'X-CMC_PRO_API_KEY': '05869e79-c079-4660-b857-57d3dd51e92e',
-//       },
-//     });
-//   } catch(ex) {
-//     response = null;
-//     // error
-//     console.log(ex);
-//     reject(ex);
-//   }
-//   if (response) {
-//     // success
-//     const json = response.data;
-//     console.log(json);
-//     resolve(json);
-//   }
-// });
-
-async function BTcToInr(){
-  let res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr")
-  let data = await res.json()
- console.log(data)
-  getTopCoin(data.bitcoin.inr)
+async function BtcToInr() {
+  let response = await fetch(
+    "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr"
+  );
+  let data = await response.json();
+  getTopCoins(data.bitcoin.inr);
 }
-BTcToInr()
 
-async function getTopCoin(btcinr){
-  let ress =await fetch("https://api.coingecko.com/api/v3/search/trending")
-  let data = await ress.json()
+BtcToInr();
+
+async function getTopCoins(BtcinInr) {
+  console.log(BtcinInr);
+  let response = await fetch(
+    "https://api.coingecko.com/api/v3/search/trending"
+  );
+  let data = await response.json();
+  getCoinDetails(BtcinInr, data.coins);
 }
+
+function getCoinDetails(BtcinInr, data) {
+  data.forEach((coinObj) => {
+    const { price_btc, thumb, name, symbol } = coinObj.item;
+    // console.log(name,price_btc,thumb);
+    const price = (price_btc * BtcinInr).toFixed(5);
+    console.log(price);
+    createCoinCard(price, thumb, name, symbol);
+  });
+}
+
+function createCoinCard(price, logo, name, symbol) {
+  //left div
+  const leftDiv = document.createElement("div");
+  const imgEle = document.createElement("img");
+
+  imgEle.src = logo;
+  imgEle.alt = "coin_logo";
+  imgEle.classList.add("card-image");
+  leftDiv.appendChild(imgEle);
+
+  //right div
+  const rightDiv = document.createElement("div");
+  // rightDiv.classList.add("");
+  //para
+  const paraEle = document.createElement("p");
+  paraEle.innerText = `₹${price}`;
+  //h1
+  const h1Ele = document.createElement("h1");
+  h1Ele.innerText = `${name} (${symbol})`;
+  rightDiv.appendChild(paraEle);
+  rightDiv.appendChild(h1Ele);
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.appendChild(leftDiv);
+    card.appendChild(rightDiv);
+    
+    document.querySelector(".top-coins").appendChild(card);
+    
+}
+
