9dd40a1eda8d6c011c3b08ddc18229ce95c8a603
Cool-Krish <61001421+Cool-Krish@users.noreply.github.com>
2023-04-28 22:37:06 +0530

    :zap: login page

---
+++ BootStrap-Projects/login.js
@@ -0,0 +1,25 @@
+let signIn = document.querySelector(".login");
+let email = document.querySelector("#inputEmail4");
+let password = document.querySelector("#inputPassword4");
+
+signIn.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let email = email.value;
+    let password = email.password;
+    const formData = new FormData(e.target);
+    console.log(formData);
+    console.log(email,password);
+    // loginUser(email, password);
+})
+
+async function loginUser(e, p) {
+    let req=await fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username: e,
+        password: p,
+      }),
+    })
+    let res = await req.json();
+    console.log(res)
+}
